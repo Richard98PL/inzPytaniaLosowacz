8337d8c21593859456a2d8ac86da6ee4976d9666
Richard98PL <41301282+Richard98PL@users.noreply.github.com>
2021-03-06 18:16:25 +0100

    Update learner.py

--- learner.py
+++ learner.py
@@ -1,18 +1,7 @@
 import random
 import time
-import os
+from os import system, name 
 import msvcrt
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 def clear(): 
     if name == 'nt': 
@@ -22,7 +11,7 @@ def clear():
 
 def sleeper(number):
     for i in range(number):
-        #print(str(number-i) + '...')
+        print(str(number-i) + '...')
         time.sleep(0.1)
     clear()
 
@@ -43,9 +32,10 @@ input_char = 'a'
 
 while ord(input_char) is not 113:
     for i in range(3):
-        print('-> ' + bcolors.WARNING + random.choice(singlePhrases) + bcolors.ENDC)
+        print('-> ' + random.choice(singlePhrases))
     print('Press anything or press q to leave...')
     input_char = msvcrt.getch()       
+    sleeper(3)
     
 
 
